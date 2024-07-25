06c88a0d728ecb59a20eaa0f362fd072ba52387d
howardbarde <howardbarde@gmail.com>
2024-07-25 18:05:18 -0400

    Added redirect page

---
+++ redirect.php
@@ -0,0 +1,4 @@
+Welcome <?php echo $_GET["name"]; ?><br>
+Your phone is: <?php echo $_GET["number"]; ?><br>
+Your email is: <?php echo $_GET["email"]; ?><br>
+Message sent: <?php echo $_GET["message"]; ?><br>
